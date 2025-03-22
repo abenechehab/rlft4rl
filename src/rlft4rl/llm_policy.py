505b9657a8932254f590132293ba2e674aeaa2f9
Abdelhakim Benechehab <abdelhakim.benechehab@gmail.com>
2025-03-22 14:43:20 +0000

    ✨ iterate over llm responses, examples in sys prompt

--- src/rlft4rl/llm_policy.py
+++ src/rlft4rl/llm_policy.py
@@ -1,3 +1,5 @@
+import logging
+from typing import List, Optional, Dict
 import re
 import numpy as np
 from openai import OpenAI
@@ -15,6 +17,8 @@ class LLMPolicy:
         logit_bias,
         good_tokens,
         env_name,
+        tol_repeat_gen: int = 10,
+        examples: Optional[Dict] = None,
     ):
         self.client = OpenAI(
             base_url=api_url,
@@ -26,9 +30,12 @@ class LLMPolicy:
         self.logit_bias = logit_bias
         self.good_tokens = good_tokens
 
-        self.init_prompt_template(env_name=env_name)
+        self.init_prompt_template(env_name=env_name, examples=examples)
 
-    def init_prompt_template(self, env_name):
+        self.tol_repeat_gen = tol_repeat_gen
+        self.n_try_gen: List[int] = []
+
+    def init_prompt_template(self, env_name: str, examples: Optional[Dict] = None):
         spec = ENV_SPECS[env_name]
 
         self.system_prompt = f"""
@@ -69,58 +76,83 @@ class LLMPolicy:
             action_dimension_6 </action>"
         5. Each action dimension value should respect the range in the action space.
 
-        Example:
-
-        <observation> {spec["example_observation"]} </observation>
-
-        <action> {spec["example_action"]} </action>
-
         """
+        if examples:
+            for i, (_, val) in enumerate(examples.items()):
+                self.system_prompt += f"""Example {i+1}:
+                <observation> {val["obs"]} </observation>
+                <action> {val["action"]} </action>
+
+                """
 
     def obs_to_prompt(self, obs):
-        prompt = f"""
-        <observation> {obs} </observation>
-
-        <action>
-        """
+        # Convert obs list to a comma-separated string without brackets
+        obs_string = ", ".join([f"{val:.4f}" for val in obs])
+        prompt = f"""<observation> {obs_string} </observation>
+        <action>"""
         return prompt
 
     def act(self, obs):
         prompt = self.obs_to_prompt(obs)
 
-        stream = self.client.chat.completions.create(
-            model=self.model,
-            messages=[
-                {"role": "system", "content": self.system_prompt},
-                {
-                    "role": "user",
-                    "content": prompt,
+        def generate_response(app_act_dim: str=""):
+            stream = self.client.chat.completions.create(
+                model=self.model,
+                messages=[
+                    {"role": "system", "content": self.system_prompt},
+                    {
+                        "role": "user",
+                        "content": prompt + app_act_dim,
+                    },
+                ],
+                temperature=self.temperature,
+                stream=True,
+                max_tokens=2 * self.prediction_horizon,
+                logit_bias={
+                    id: 30 if self.logit_bias else 0 for id in self.good_tokens
                 },
-            ],
-            temperature=self.temperature,
-            stream=True,
-            max_tokens=2 * self.prediction_horizon,
-            logit_bias={id: 30 if self.logit_bias else 0 for id in self.good_tokens},
-        )
+            )
 
-        raw_response = ""
-        for chunk in stream:
-            try:
-                raw_response += chunk.choices[0].delta.content
-            except TypeError:
-                pass
-        full_response = raw_response.split(" </action>")[0]
-        full_response = full_response.split(",")
+            raw_response = ""
+            for chunk in stream:
+                try:
+                    raw_response += chunk.choices[0].delta.content
+                except TypeError:
+                    pass
+            full_response = app_act_dim + raw_response.split(" </action>")[0]
+            full_response = full_response.split(",")
 
-        if full_response[0] == "":
-            full_response = full_response[1:]
+            if full_response[0] == "":
+                full_response = full_response[1:]
 
-        # Filter to keep only alphanumeric, -, and . characters
-        filtered_response = [re.sub(r"[^0-9\-\.]", "", item) for item in full_response]
+            # Filter to keep only alphanumeric, -, and . characters
+            filtered_response = [
+                re.sub(r"[^0-9\-\.]", "", item) for item in full_response
+            ][:6]
+            return raw_response, filtered_response
 
-        assert (
-            len(filtered_response) == 6
-        ), f"Expected 6 action dimensions, got {filtered_response}"
+        count = 0
+        app_act_dim = ""
+        while True:
+            raw_response, filtered_response = generate_response(app_act_dim=app_act_dim)
+            if len(filtered_response) == 6:
+                self.n_try_gen.append(count+1)
+                break
+            else:
+                app_act_dim = ", ".join(filtered_response) + ", "
+            count += 1
+            if count > self.tol_repeat_gen:
+                raise Exception(
+                    "Failed to get a valid response from the model! "
+                    f"Expected 6 action dimensions, got raw: {raw_response},"
+                    f"filtered: {filtered_response}"
+                )
 
         action = [float(x) for x in filtered_response]
         return np.array(action)
+
+    def log(self, logger: logging.Logger):
+        logger.info(f"[N tries gen] mean{np.mean(self.n_try_gen)}")
+        logger.info(f"[N tries gen] std{np.std(self.n_try_gen)}")
+        logger.info(f"[N tries gen] min{np.min(self.n_try_gen)}")
+        logger.info(f"[N tries gen] max{np.max(self.n_try_gen)}")
